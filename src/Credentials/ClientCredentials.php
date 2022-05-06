04a4ff8e9e34c2df20b900ccc1be48d3a23fb563
Christoph Kappestein <christoph.kappestein@gmail.com>
2022-05-06 20:57:26 +0200

    update php docs

--- src/Credentials/ClientCredentials.php
+++ src/Credentials/ClientCredentials.php
@@ -3,7 +3,7 @@
  * SDKgen is a tool to automatically generate high quality SDKs.
  * For the current version and information visit <https://sdkgen.app>
  *
- * Copyright 2020-2021 Christoph Kappestein <christoph.kappestein@gmail.com>
+ * Copyright 2020-2022 Christoph Kappestein <christoph.kappestein@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
